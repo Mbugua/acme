b42b282668976544118810936b99da8afeec8faa
Lee Mbuuga <mbugua.dev@gmail.com>
2019-12-03 11:29:43 +0300

    fix rating issue

--- booking/urls.py
+++ booking/urls.py
@@ -1,4 +1,3 @@
-# from django.conf import Settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
@@ -9,4 +8,7 @@ urlpatterns = [
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^bus-details/(\d+)', views.bus_details, name='bus_details'),
     url(r'^mobile_payment/(\d+)', views.mobile_payment, name='mobile_payment'),
+    url(r'^ratings/', include('star_ratings.urls',
+                              namespace='ratings', app_name='ratings')),
+    url(r'^ticket/(?P<ticket_id>(\d+))$', generate_view),
 ]
